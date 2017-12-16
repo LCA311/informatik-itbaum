39509f41fca70374783acc0b60afc22ed8d4ec14
LCA311 <spitzer-webdesign@outlook.de>
2017-12-16 12:37:44 +0100

    PHP hinzugefügt

---
+++ php/add.php
@@ -0,0 +1,30 @@
+<?php
+
+	require_once('../dbconfig.php');
+
+	$db = new mysqli(dbhost, dbuser, dbpass, dbname);
+
+	if ($db->connect_error)
+		die("-connection failed: ".$db->connect_error);
+
+	$text = $db->real_escape_string($_GET['text']);
+	$posX = $db->real_escape_string($_GET['x']);
+	$posY = $db->real_escape_string($_GET['y']);
+	$thema = $db->real_escape_string($_GET['thema']);
+
+	$sql = "INSERT INTO Thema VALUES (null, '".$thema."') ON DUPLICATE KEY UPDATE name=name";
+	$sql2 = "INSERT INTO Nodes VALUES (null, '".$text."', ".$posX.", ".$posY.", (SELECT id FROM Thema WHERE name='".$thema."'))";
+
+	$result = $db->query($sql);
+
+	if($result === false)
+		die("-ERR 1");
+
+	$result = $db->query($sql2);
+
+	if($result === false)
+		die("-ERR 2");
+
+	echo "+OK";
+
+?>
